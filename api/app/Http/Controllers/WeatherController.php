ffc86796f9b6237402e7d038cea09759a6da7e9a
hmuhammed <hm@dicube.at>
2023-03-04 18:13:21 +0500

    creating laravel job and picking data in controller from the cache

--- api/app/Http/Controllers/WeatherController.php
+++ api/app/Http/Controllers/WeatherController.php
@@ -3,72 +3,34 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Cache;
-use Illuminate\Support\Facades\Http;
 
 class WeatherController extends Controller
 {
-    function weather(){
+    public function weather(){
         $users = User::all();
         $weatherData = [];
-
         foreach ($users as $user) {
             $latitude = $user->latitude;
             $longitude = $user->longitude;
             $cachedData = Cache::get('weather-' . $latitude . '-' . $longitude);
-            if ($cachedData) {
-                $cachedTimestamp = $cachedData['timestamp'];
-                if (time() - $cachedTimestamp < 3600) {
-                    $weatherData[] = [
-                        'user' => $user->toArray(),
-                        'weather' => $cachedData['data'],
-                    ];
-                    continue;
-                }
-            }
-
-            try {
-                $response = Http::timeout(0.5)->get('https://api.openweathermap.org/data/2.5/weather', [
-                    'lat' => $latitude,
-                    'lon' => $longitude,
-                    'appid' => env('OPENWEATHERMAP_API_KEY'),
-                    'units' => 'metric',
-                ]);
-            } catch (\Illuminate\Http\Client\RequestException $e) {
-
-                abort(500, 'Failed to retrieve weather data: ' . $e->getMessage());
-            }
-
-            if ($response->failed()) {
-                abort(500, 'Failed to retrieve weather data.');
-            }
-
             $weatherData[] = [
                 'user' => $user->toArray(),
-                'weather' => $response->json(),
+                'weather' => $cachedData['data'],
             ];
-            Cache::put('weather-' . $latitude . '-' . $longitude, [
-                'data' => $response->json(),
-                'timestamp' => time()
-            ], now()->addHour());
         }
-
         $formattedData = [];
-
         foreach ($weatherData as $data) {
-            $formattedData[] = [
+            $formattedData = [
                 'id' => $data['user']['id'],
                 'name' => $data['user']['name'],
                 'email' => $data['user']['email'],
-                'email_verified_at' => $data['user']['email_verified_at'],
-                'created_at' => $data['user']['created_at'],
-                'updated_at' => $data['user']['updated_at'],
                 'latitude' => $data['user']['latitude'],
                 'longitude' => $data['user']['longitude'],
                 'description' => $data['weather']['weather'][0]['description'],
                 'location' => $data['weather']['name'],
                 'temperature' => $data['weather']['main']['temp'],
+                'user_weather'=>$data['weather']
             ];
         }
         return [
---
+++ api/app/Jobs/UpdateUserWeatherInfo.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\User;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
+
+class UpdateUserWeatherInfo implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        $users = User::all();
+        $weatherData = [];
+        foreach ($users as $user) {
+            $latitude = $user->latitude;
+            $longitude = $user->longitude;
+            $cachedData = Cache::get('weather-' . $latitude . '-' . $longitude);
+            if ($cachedData) {
+                $cachedTimestamp = $cachedData['timestamp'];
+                if (time() - $cachedTimestamp < 3600) {
+                    $weatherData[] = [
+                        'user' => $user->toArray(),
+                        'weather' => $cachedData['data'],
+                    ];
+                    continue;
+                }
+            }
+            try {
+                $response = Http::timeout(0.5)->get('https://api.openweathermap.org/data/2.5/weather', [
+                    'lat' => $latitude,
+                    'lon' => $longitude,
+                    'appid' => env('OPENWEATHERMAP_API_KEY'),
+                    'units' => 'metric',
+                ]);
+            } catch (\Illuminate\Http\Client\RequestException $e) {
+                Log::info($e->getMessage());
+                abort(500, 'Failed to retrieve weather data: ' . $e->getMessage());
+            }
+
+            if ($response->failed()) {
+                Log::info('Failed to retrieve weather data.');
+                abort(500, 'Failed to retrieve weather data.');
+            }
+
+            $weatherData[] = [
+                'user' => $user->toArray(),
+                'weather' => $response->json(),
+            ];
+            Cache::put('weather-' . $latitude . '-' . $longitude, [
+                'data' => $response->json(),
+                'timestamp' => time()
+            ], now()->addHour());
+        }
+    }
+}
